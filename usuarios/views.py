71c95309e47914327f13d6629c8ddfab6636147c
fps_netodf <fps_netodf@hotmail.com>
2024-04-18 15:48:32 -0300

    add_messages, constants

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.messages import constants, add_message
+
 
 # Create your views here.
 
@@ -10,5 +13,16 @@ def cadastro(request):
     # print(request.META)
     return HttpResponse("Olá, mundo!")
 
-def login(request):
-    return render(request, "usuarios/login.html")
+def login_view(request):
+    if request.method == "GET":
+        return render(request, 'usuarios/login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get("senha")
+        user = authenticate(request, username=username, password=senha)
+        if user:
+            login(request, user)
+            add_message(request, constants.ERROR, 'Usuário ou senha incorretos')
+            return redirect('/pacientes/home')
+        return redirect('/login')
+
