a7690780de1677e79c1e14a83db15c3afb7ad1fa
fps_netodf <fps_netodf@hotmail.com>
2024-04-22 14:00:21 -0300

    quase finalizado

--- medico/views.py
+++ medico/views.py
@@ -73,7 +73,7 @@ def abrir_horario(request):
     if request.method == "GET":
         dados_medicos = DadosMedico.objects.get(user=request.user)
         datas_abertas = DatasAbertas.objects.filter(user=request.user)
-        return render(request, 'abrir_horario.html', {'dados_medicos': dados_medicos, 'datas_abertas': datas_abertas, 'is_medico': is_medico(request.user)})
+        return render(request, 'medico/abrir_horario.html', {'dados_medicos': dados_medicos, 'datas_abertas': datas_abertas, 'is_medico': is_medico(request.user)})
     elif request.method == "POST":
         data = request.POST.get('data')
         data_formatada = datetime.strptime(data, "%Y-%m-%dT%H:%M")
--- pacientes/views.py
+++ pacientes/views.py
@@ -18,14 +18,14 @@ def home(request):
             medicos = medicos.filter(nome__icontains = medico_filtrar)
         if especialidades_filtrar:
             medicos = medicos.filter(especialidade_id__in=especialidades_filtrar)
-            return render(request, 'pacientes/home.html', {'medicos': medicos, 'especialidades': especialidades, 'is_medico': is_medico(request.user)})
+        return render(request, 'pacientes/home.html', {'medicos': medicos, 'especialidades': especialidades, 'is_medico': is_medico(request.user)})
 
 
 def escolher_horario(request, id_dados_medicos):
     if request.method == "GET":
         medico = DadosMedico.objects.get(id=id_dados_medicos)
         datas_abertas = DatasAbertas.objects.filter(user=medico.user).filter(data__gte=datetime.now()).filter(agendado=False)
-        return render(request, 'escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas, 'is_medico': is_medico(request.user)})
+        return render(request, 'pacientes/escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas, 'is_medico': is_medico(request.user)})
     
 def agendar_horario(request, id_data_aberta):
     if request.method == "GET":
@@ -45,7 +45,7 @@ def minhas_consultas(request):
     if request.method == "GET":
         #TODO: desenvolver filtros
         minhas_consultas = Consulta.objects.filter(paciente=request.user).filter(data_aberta__data__gte=datetime.now())
-        return render(request, 'minhas_consultas.html', {'minhas_consultas': minhas_consultas,})
+        return render(request, 'pacientes/minhas_consultas.html', {'minhas_consultas': minhas_consultas,})
     
 
 def consultas_medico(request):
